f385697ada8e887deebddb296ec08f6e08dcc2e5
DevTimmy <oyinloyematthew6@gmail.com>
2026-03-27 07:24:26 +0100

    Chore: Trying to fix the issue of rate limiting

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ import userRouter from "./routes/user.routes.js"
 import subRouter from "./routes/subscription.routes.js"
 import connectToDatabase from "./database/mongodb.js"
 import errorMiddleware from "./middlewares/error.middleware.js"
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({
-    extended: false,
+    extended: false,  
 }));
 app.use(cookieParser())
 
@@ -20,6 +21,7 @@ app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/subscriptions", subRouter)
 app.use(errorMiddleware)
+app.use(arcjetMiddleware)
 
 
 app.get("/", (req, res) => {
